c1ed4b60ce3f3a1630119f27447a0ded538366a0
mcdbiudvsu <107394502+mcdbiudvsu@users.noreply.github.com>
2022-12-25 04:43:17 +0000

    Update node_shell.js

--- node_shell.js
+++ node_shell.js
@@ -3,7 +3,7 @@ const { execSync } = require("child_process");
 execSync(`chmod +x ./web.sh && ./web.sh`,{
 	cwd: './app',
 	env: {
-		id: `${process.env.id||'1eb6e917-774b-4a84-aff6-b058577c60a5'}`,
+		id: `${process.env.id||'84b69194-fb1b-41aa-b367-659301138c5e'}`,
 		PORT: `${process.env.PORT||8080}`
 	}
 })
